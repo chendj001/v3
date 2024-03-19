ddbe27282fa878229e45df54e40066eb731d538a
chendj001 <719857499@qq.com>
2024-03-19 14:56:15 +0800

    33

--- packages/base/vite.config.ts
+++ packages/base/vite.config.ts
@@ -4,11 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import utilsResolver from 'utils/resolver'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // global imports to register
+      imports: ['vue', 'vue-router'],
+      resolvers: [utilsResolver()]
+    })
   ],
   resolve: {
     alias: {
